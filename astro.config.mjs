ee160f173bf2b11f2c922030b5c81aa78d4bf95d
Anav Chand <anavkki@gmail.com>
2023-06-19 09:40:07 +1200

    Astro Sitemap intergration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import image from '@astrojs/image';
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+    site: "https://codeology.net",
   integrations: [tailwind(), image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  })]
+  }), sitemap()]
 });
